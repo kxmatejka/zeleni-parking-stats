a8ffc234834b1514f862bb06c02cc4bf8c93a98d
Kryštof Matějka <krystof.matejka@pipedrive.com>
2023-05-06 11:31:09 +0200

    init kv

--- pages/api/process-data.ts
+++ pages/api/process-data.ts
@@ -1,4 +1,13 @@
-export default function handler(req, res) {
-	console.log('executed');
+import kv from "@vercel/kv";
+
+export default async function handler(req, res) {
+	console.log('started');
+
+	await await kv.set('key', new Date().toString());
+
+	const redisRes = await kv.get('key');
+
+	console.log('res', redisRes);
+
   res.status(200).json({ name: 'John Doe' });
 }
