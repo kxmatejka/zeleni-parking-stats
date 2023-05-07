d76ef498336b30cb51d9e346dab6d316056b2d44
Kryštof Matějka <krystof.matejka@pipedrive.com>
2023-05-07 18:11:49 +0200

    save data async

--- lib/parking/save-parking.ts
+++ lib/parking/save-parking.ts
@@ -8,7 +8,9 @@ const config = {
 const connection = connect(config);
 
 export async function saveParking(parking: Parking[]) {
-	for (const record of parking) {
-		await connection.execute("INSERT INTO parking_spaces (name, code, city, total, free) VALUES (?, ?, ?, ?, ?)", [record.name, record.code, record.city, record.total, record.free]);
-	}
+	await Promise.all(
+		parking.map(
+			(record) => connection.execute("INSERT INTO parking_spaces (name, code, city, total, free) VALUES (?, ?, ?, ?, ?)", [record.name, record.code, record.city, record.total, record.free])
+		)
+	);
 }
