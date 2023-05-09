f3ffa4dd431f6772cd466dded6bc852dc2372743
Kryštof Matějka <krystof.matejka@pipedrive.com>
2023-05-09 14:50:54 +0200

    save safely

--- lib/parking/save-parking.ts
+++ lib/parking/save-parking.ts
@@ -7,10 +7,14 @@ const config = {
 
 const connection = connect(config);
 
+async function save(record: Parking) {
+	try {
+		await connection.execute("INSERT INTO parking_spaces (name, code, city, total, free) VALUES (?, ?, ?, ?, ?)", [record.name, record.code, record.city, record.total, record.free]);
+	} catch (error) {
+		console.error("Failed to save", error);
+	}
+}
+
 export async function saveParking(parking: Parking[]) {
-	await Promise.all(
-		parking.map(
-			(record) => connection.execute("INSERT INTO parking_spaces (name, code, city, total, free) VALUES (?, ?, ?, ?, ?)", [record.name, record.code, record.city, record.total, record.free])
-		)
-	);
+	await Promise.all(parking.map((record) => save(record)));
 }
